381ddc35fe1455667c9de9689c7e745763336d63
HYPER-CEC101 <joesongyy@gmail.com>
2021-01-19 20:14:00 +0800

    20210119_Update settings.py make it allowed all hosts

--- src/cec101/settings.py
+++ src/cec101/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#l7vxayrqq(prm@kkavn$t9r#m^mso7phrj(j^3f0o1*cwbtcn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
